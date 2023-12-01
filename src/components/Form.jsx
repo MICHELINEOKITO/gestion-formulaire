f352901fbe314a4d8a6b6e5823faf18253069b3d
Micheline-Ok <okitobibi2@gmail.com>
2023-12-01 14:36:38 +0100

    Gestion d'entréer des informations des utilisateurs avec react

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,10 +8,16 @@ const Form = () => {
          const [data, setData] = useState ({name:"", email:"", phone:"", message: ""});
          // data est un objet qui a 4 propriétées
 
-        const handleChange = () =>{
+        const handleChange = (e) =>{
+            const name = e.target.name;
+            const value= e.target.value;
+            setData({...data,[name]: value})
+
 
     }
-        const handleSubmit = () =>{
+        const handleSubmit = (e) =>{
+            e.preventDefault();
+            alert(data)
 
         }
     
@@ -21,10 +27,12 @@ const Form = () => {
             <h1>Contactez- <span>nous</span></h1>
             <input type="text" name="name" id="" onChange={handleChange} value={data.name}  placeholder="Entrer votre nom"/>
             <input type="email" name="email" id="" onChange={handleChange} value={data.email}placeholder="michline.okito@gmail.com"/>
-            <input type="phone" name="phone" id=""  onChange={handleChange} value={data.phone}placeholder="+243"/>
-            <textarea name="" id="" cols="30" rows="10"  onChange={handleChange} value={data.message} placeholder="Taper ici..."/>
+            <input type="number" name="phone" id=""  onChange={handleChange} value={data.phone}placeholder="+243"/>
+            <textarea name="message" id="" cols="30" rows="10"  onChange={handleChange} value={data.message} placeholder="Taper ici..."/>
             <button type="submit">Envoyer</button>
+        <p>{data.name},{data.email},{data.phone},{data.message}</p>
         </form>
+       
 
         </>
 
