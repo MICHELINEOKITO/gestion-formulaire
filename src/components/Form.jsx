6b1e5c37a8400c5ab28a86c518be046d62eea57e
Micheline-Ok <okitobibi2@gmail.com>
2023-12-01 14:04:55 +0100

    initialisation et affichage de different champ de formulaire

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+import { useState } from "react";
 
 
 const Form = () => {
+    // ce composant form va nous permettre d'afficher l'UI du formul et gérer les entrés des utilisateurs
+
+         const [data, setData] = useState ({name:"", email:"", phone:"", message: ""});
+         // data est un objet qui a 4 propriétées
+
+        const handleChange = () =>{
+
+    }
+        const handleSubmit = () =>{
+
+        }
+    
     return(
         <>
-        <form>
+        <form method="post" onSubmit={handleSubmit}>
             <h1>Contactez- <span>nous</span></h1>
-            <input type="text" name="name"  placeholder="Entrer votre nom"/>
-            <input type="email" name="email" id="" placeholder="michline.okito@gmail.com"/>
-            <input type="phone" name="phone" id="" placeholder="+243"/>
-            <textarea name="" id="" cols="30" rows="10" placeholder="Taper ici..."/>
+            <input type="text" name="name" id="" onChange={handleChange} value={data.name}  placeholder="Entrer votre nom"/>
+            <input type="email" name="email" id="" onChange={handleChange} value={data.email}placeholder="michline.okito@gmail.com"/>
+            <input type="phone" name="phone" id=""  onChange={handleChange} value={data.phone}placeholder="+243"/>
+            <textarea name="" id="" cols="30" rows="10"  onChange={handleChange} value={data.message} placeholder="Taper ici..."/>
             <button type="submit">Envoyer</button>
         </form>
 
